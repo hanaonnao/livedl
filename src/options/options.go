1552e7c93b33504d84c6367d1d3adab7cf2758b9
Yukikazehana <hanaonnao@yahoo.com.tw>
2020-09-03 23:43:47 +0800

    Update options.go

--- src/options/options.go
+++ src/options/options.go
@@ -158,6 +158,18 @@ HTTP関連
   -http-skip-verify=on           (+) TLS証明書の認証をスキップする (32bit版対策)
   -http-skip-verify=off          (+) TLS証明書の認証をスキップしない (デフォルト)
 
+追加オプション
+　-nico-conv-seqno-start ＜num＞  MP4への変換を指定したセグメント番号から開始する
+　-nico-conv-seqno-end ＜num＞  　MP4への変換を指定したセグメント番号で終了する
+　-nico-conv-force-concat     　　MP4への変換で画質変更または抜けがあっても分割しないように設定
+　-nico-conv-force-concat=on     (+) 上記を有効に設定
+　-nico-conv-force-concat=off    (+) 上記を無効に設定(デフォルト)
+ 
+追加オプション [実験的]
+　-d2h
+     録画済みのdb(.sqlite3)を視聴するためのHLSサーバを立てる(-db-to-hls)
+　　　開始シーケンス番号は（変換ではないが） -nico-conv-seqno-start で指定
+     使用例：$ livedl lvXXXXXXXXX.sqlite3 -d2h -nico-hls-port 12345 -nico-conv-seqno-start 2780
 
 (+)のついたオプションは、次回も同じ設定が使用されることを示す。
 
